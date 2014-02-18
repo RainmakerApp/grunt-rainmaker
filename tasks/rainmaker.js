65963e43c86dfed1c830d77a796c459782d647c5
David Boskovic <me@david.gs>
2014-02-18 15:04:17 -0800

    first commit

---
+++ tasks/rainmaker.js
@@ -0,0 +1,116 @@
+/*
+ * grunt-rainmaker
+ *
+ * Copyright (c) 2013 David Boskovic
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+module.exports = function (grunt) {
+
+
+  var crypto  = require('crypto'),
+      rest    = require('restler'),
+      jade    = require('jade'),
+      _       = require('underscore'),
+      path    = require('path');
+
+
+
+    var sendToRainmaker = function(f,source,options,ff,cb) {
+      if(f.match(/.+\.jade/)) {
+        source = (jade.compile(source, {filename:f.replace('.jade','.lhtml'),pretty:true}))({});
+      }
+      ff = ff.replace('.html','.lhtml').replace('.jade','.lhtml');
+
+      rest.post((options.domain || 'http://'+options.client+'.donate.io')+'/apiv2/themes/'+options.theme+'/files?--suppress-content', {
+        multipart: false,
+        username: options.key,
+        password: options.secret,
+        data: {
+          'file': ff,
+          'content': new Buffer(source).toString('base64')
+        }
+      }).on('complete', function(data) {
+        grunt.log.writeln('Theme File ' + ff.cyan + ' sent to server.');
+        cb(null,data);
+      });
+    }
+
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('rainmaker', 'Interact with the RainmakerApp API', function () {
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    // var options = this.options({
+    //   key: false,
+    //   secret: false,
+    //   domain:false,
+    //   theme:false
+    // });
+
+
+    var options = this.options({
+      length: 32,
+      hashmap: '.hash',
+      changes: '.cache/',
+      key: false,
+      secret: false,
+      client:false,
+      theme:'default'
+    }),
+      hashMap = grunt.file.exists(options.hashmap) ? grunt.file.readJSON(options.hashmap) : {}, // Read the backup hash or empty
+      cwd = this.data.cwd,
+      tmpHash = {}, // Save current files's hash
+      changeList = [],
+      flow = [],
+      v;
+
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function (f) {
+      f.src.filter(function (filepath) { // Ignore dirs
+        return !grunt.file.isDir(filepath);
+      }).forEach(function (src) {
+        var source = grunt.file.read(src, {
+          encoding: 'utf8'
+        }),
+          hash = crypto.createHash('md5').update(source).digest('hex').slice(0, options.length);
+        tmpHash[src] = hash;
+      });
+    });
+
+    // Filter the changes
+    _.each(tmpHash,function(item,k){
+      if(typeof hashMap[k] === 'undefined' || item !== hashMap[k]) {
+        changeList.push(k);
+      }
+    })
+    
+
+    if(changeList.length > 0) {
+
+      // Copy changed files
+      changeList.forEach(function (file) {
+        // grunt.file.copy(file, options.changes + path.relative(cwd, file));
+        // send file to rainmaker
+        var source = grunt.file.read(file, {
+          encoding: 'utf8'
+        })
+        flow.push(function(cb){
+          sendToRainmaker(file,source,options,path.relative(cwd, file),cb);
+        })
+
+        grunt.log.writeln('Detect file ' + file.cyan + ' has been changed.');
+      });
+      grunt.util.async.series(flow, this.async());
+      grunt.file.write(options.hashmap, JSON.stringify(tmpHash));
+      grunt.log.writeln('Hashmap ' + options.hashmap.cyan + ' created/updated.');
+    } else {
+      grunt.log.writeln('No file has been changed, do nothing.');
+    }
+  });
+
+};
